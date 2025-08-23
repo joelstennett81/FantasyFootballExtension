b1cfbfb9d4e33a900a1899091ce0710b781215ba
Joel Stennett <joelstennett17@gmail.com>
2025-08-23 10:56:47 -0400

    added env and changed login

---
+++ fantasy_football_extension/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    username = forms.EmailField(label="Email", required=True)
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fieldname in self.fields:
+            self.fields[fieldname].help_text = None
--- fantasy_football_extension/views.py
+++ fantasy_football_extension/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .forms import CustomUserCreationForm
 from .utils import calculate_vorp_and_voas
 
 
@@ -16,17 +17,17 @@ def home_page(request):
 
 
 # Simple register form
+
 def register_view(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect("home_page")
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, "register.html", {"form": form})
-
 def logout_view(request):
     logout(request)
     return redirect("home_page")
